ce7cd30d0ee032c8438433ea6ebd20e0875a3b36
jujubeliao <tendaysling@gmail.com>
2023-01-13 16:27:59 +0800

    add json-server-auth & set db.json

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // https://github.com/typicode/json-server#module
+// https://github.com/jeremyben/json-server-auth#module-usage-
+
 
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+server.db = router.db
+
 server.use(middlewares)
+server.use(auth)
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
